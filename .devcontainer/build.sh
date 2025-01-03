b21b3090248a22b833d88584cfa5497dc0fb753b
Michele Sciabarra <michele@nuvolaris.io>
2025-01-03 22:59:14 +0100

    fix build

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,7 +5,8 @@ TOKEN=${GITHUB_TOKEN:?please set GITHUB_TOKEN}
 docker login ghcr.io -u mastrogpt -p $TOKEN
 docker buildx create --use --name mybuilder
 docker buildx inspect --bootstrap
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --privileged --rm tonistiigi/binfmt --install all
+
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
   -t $IMAGE:$VERSION . --push
