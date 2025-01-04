62b5a11a362b35fcff4a33c7dc9ec3376b228452
Michele Sciabarra <michele@nuvolaris.io>
2025-01-04 15:26:43 +0100

    Dockerfile

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-VERSION=2501032215
-IMAGE=ghcr.io/mastrogpt/mastrogpt-starter
-TOKEN=${GITHUB_TOKEN:?please set GITHUB_TOKEN}
+source .env
+TAG=2501032215
+DH_USER=sciabarracom
+DH_BUILDER=openserverless-builder
+GH_IMAGE=ghcr.io/mastrogpt/mastrogpt-starter
+DH_IMAGE=$DH_USER/mastrogpt-starter
+GH_TOKEN=${GITHUB_TOKEN:?please set GITHUB_TOKEN in .env}
+DH_TOKEN=${DOCKERHUB_TOKEN:?please set DOCKERHUB_TOKEN in .env} 
+docker login -u $DH_USER -p "$DH_TOKEN"
+docker buildx create  --driver cloud $DH_USER/$DH_BUILDER
+
 docker login ghcr.io -u mastrogpt -p $TOKEN
 docker buildx create --use --name mybuilder
 docker buildx inspect --bootstrap
