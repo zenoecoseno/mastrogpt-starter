592d5bd1d63e67df467555cae42530acac977c92
Michele Sciabarra <msciabarra@apache.org>
2025-01-05 20:10:55 +0100

    updating build

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname $0)"
-source ../.env
-TAG=2501040800
+source .env
+TAG=2501052000
 DH_USER=sciabarracom
 DH_BUILDER=openserverless-builder
 DH_IMAGE=$DH_USER/mastrogpt-starter
@@ -12,5 +12,13 @@ docker buildx build --no-cache \
   --builder cloud-$DH_USER-$DH_BUILDER \
   --platform linux/amd64,linux/arm64 \
   -t $DH_IMAGE:$TAG . --push
+<<<<<<< HEAD
 
+=======
+#GH_IMAGE=ghcr.io/mastrogpt/mastrogpt-starter
+#GH_TOKEN=${GITHUB_TOKEN:?please set GITHUB_TOKEN in .env}
+#docker pull $DH_IMAGE:$TAG
+#docker tag $DH_IMAGE:$TAG $GH_IMAGE:$TAG
+#docker push $GH_IMAGE:$TAG
+>>>>>>> 9561796 (updating build)
 
