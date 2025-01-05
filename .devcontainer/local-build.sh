510aaff228963f2f83c3b43ac27a3bca2eacc36a
Michele Sciabarra <msciabarra@apache.org>
2025-01-05 21:24:56 +0100

    dockerfile withouth get-ops-lates

--- .devcontainer/local-build.sh
+++ .devcontainer/local-build.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 cd "$(dirname $0)"
 source .env
-TAG=2501052000
+TAG=$(jq -r .image <devcontainer.json | awk -F: '{print $2}')
 DH_USER=sciabarracom
 DH_BUILDER=openserverless-builder
 DH_IMAGE=$DH_USER/mastrogpt-starter
 DH_TOKEN=${DOCKERHUB_TOKEN:?please set DOCKERHUB_TOKEN in .env} 
-docker login -u $DH_USER -p "$DH_TOKEN"
+docker login -u "$DH_USER" -p "$DH_TOKEN"
 docker buildx create  --driver cloud $DH_USER/$DH_BUILDER
-docker buildx build --no-cache \
+nohup docker buildx build --no-cache \
   --builder cloud-$DH_USER-$DH_BUILDER \
   --platform linux/amd64,linux/arm64 \
-  -t $DH_IMAGE:$TAG . --push
-<<<<<<< HEAD
-
-=======
-#GH_IMAGE=ghcr.io/mastrogpt/mastrogpt-starter
-#GH_TOKEN=${GITHUB_TOKEN:?please set GITHUB_TOKEN in .env}
-#docker pull $DH_IMAGE:$TAG
-#docker tag $DH_IMAGE:$TAG $GH_IMAGE:$TAG
-#docker push $GH_IMAGE:$TAG
->>>>>>> 9561796 (updating build)
-
+  -t $DH_IMAGE:$TAG . --push &
+tail -f nohup.out
