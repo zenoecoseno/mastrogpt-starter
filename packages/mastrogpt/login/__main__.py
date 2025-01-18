5ecb4b298f420b496f8424862e196dc1dc8ce263
Michele Sciabarra <msciabarra@apache.org>
2025-01-18 19:20:10 +0100

    updated login

--- packages/mastrogpt/login/__main__.py
+++ packages/mastrogpt/login/__main__.py
@@ -1,26 +1,8 @@
-#--kind python:default
+# --kind python:default
+#--docker docker.io/apache/openserverless-runtime-python:v3.12-2501172243
 #--web true
-#--param USERNAME $OPSDEV_USERNAME
-#--param HOST $OPSDEV_HOST
 #--param S3_SECRET_KEY $S3_SECRET_KEY
-#--param MASTROGPT_USERNAME $MASTROGPT_USERNAME
-#--param MASTROGPT_PASSWORD $MASTROGPT_PASSWORD
-import os, json
-from pathlib import Path
-
+#--annotation provide-api-key true
+import login
 def main(args):
-    
-    username = args.get("username")
-    password = args.get("password")
-    username_check = args.get("MASTROGPT_USERNAME")
-    password_check= args.get("MASTROGPT_PASSWORD")
-    print("check", username_check, password_check)
-    res = { "authenticated": False}
-    if username_check != "" and password_check != "":
-        if username == username_check and password ==  password_check:
-            res = {
-                "authenticated": True,
-                "s3_key": args.get("S3_SECRET_KEY", "")
-            }
-    return { "body": res }
-  
+    return {"body": login.login(args)}
