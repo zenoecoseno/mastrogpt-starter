dd024f7aba6a90ff694471d114efbe466935c04c
Michele Sciabarra <msciabarra@apache.org>
2025-01-19 13:08:39 +0100

    updated image

--- .devcontainer/local-build.sh
+++ .devcontainer/local-build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 cd "$(dirname $0)"
+set -a
 source .env
-TAG=$(jq -r .image <devcontainer.json | awk -F: '{print $2}')
+TAG=$(grep 'image: sciabarracom/mastrogpt-starter' docker-compose.yml | awk -F: '{print $3}')
 LATEST=https://api.github.com/repos/apache/openserverless-cli/releases/latest
-OPS_VERSION=$(curl -s  "$LATEST" | jq -r '.name | .[1:]')
+OPS_VERSION=$(curl -s "$LATEST" | jq -r '.name | .[1:]')
 DH_USER=sciabarracom
 DH_BUILDER=openserverless-builder
 DH_IMAGE=$DH_USER/mastrogpt-starter
 DH_TOKEN=${DOCKERHUB_TOKEN:?please set DOCKERHUB_TOKEN in .env} 
 docker login -u "$DH_USER" -p "$DH_TOKEN"
 docker buildx create  --driver cloud $DH_USER/$DH_BUILDER
-nohup docker buildx build --no-cache \
+docker buildx build --no-cache \
   --build-arg OPS_VERSION=$OPS_VERSION \
   --builder cloud-$DH_USER-$DH_BUILDER \
   --platform linux/amd64,linux/arm64 \
-  -t $DH_IMAGE:$TAG . --push &
-tail -f nohup.out
+  -t $DH_IMAGE:$TAG . --push
--- .devcontainer/trigger-build.sh
+++ .devcontainer/trigger-build.sh
@@ -1,4 +1,4 @@
-TAG=$(jq -r .image <devcontainer.json | awk -F: '{print $2}')
+TAG=$(grep 'image: sciabarracom/mastrogpt-starter' docker-compose.yml | awk -F: '{print $3}')
 git commit -m "trigger build for $TAG" -a
 git tag $TAG
 echo Ready for $TAG - run 'git  push origin main --tags'
